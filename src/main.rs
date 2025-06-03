c39d8feba0ba9b0a8738c30c5a8757dbd8d0dcf4
abdulwahed.mansour <abdulwahed.mansour@gmail.com>
2025-06-03 01:25:42 +0000

    read cars.json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use rand::Rng;
+
+use std::fs;
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let n: u8 = rng.gen_range(1..=100);
-    println!("Generated random number: {}", n);
-    println!("Hello, me!");
+    let data_file_path = "cars.json";
+    match fs::read_to_string(data_file_path) {
+        Ok(data) => {
+            println!("Contents of {}:", data_file_path);
+            println!("{}", data);
+        }
+        Err(e) => {
+            eprintln!("Error reading {}: {}", data_file_path, e);
+        }
+    }
 }
